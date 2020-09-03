8a4c6d5ad4c39f8efe22238dddd42632daf93fc9
tacostahern@gmail.com <tacostahern@gmail.com>
2020-09-03 15:24:05 -0500

    Basic AccountHolder program done, still needs commenting and error trapping

--- Labs/Lab1/AccountHolder.java
+++ Labs/Lab1/AccountHolder.java
@@ -1,4 +1,25 @@
 public class AccountHolder
 {
-    
+    public static double annualInterestRate;
+    public double balance;
+
+    public AccountHolder(double balance)
+    {
+        this.balance = balance; //Need error message for negative balance
+    }
+
+    public void deposit(double depo)
+    {
+        balance += deposit; //Adds deposit to our balance
+    }
+
+    public void withdrawal(double with)//balance cannot drop below $50
+    {
+        balance -= with; //Need error trapping
+    }
+
+    public void MonthlyInterest()//Calculates our monthly interest
+    {
+        balance += balance * (annualInterestRate / 12.0)
+    }
 }
