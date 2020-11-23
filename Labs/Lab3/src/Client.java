a7eae1ec65e0b6936931b1ea1f4f3a2b00877031
tacostahern@gmail.com <tacostahern@gmail.com>
2020-11-22 19:02:26 -0600

    Added entire lab

---
+++ Labs/Lab3/src/Client.java
@@ -0,0 +1,10 @@
+/*
+Author: Tony Acosta Hernandez
+Course: ITMD 411
+Program description: This program contains the abstract class Client, which has 3 methods: readData(), processData(), printData(), which will be implemented in any programs that extend Client
+*/
+public abstract class Client {
+    public abstract void readData();
+    public abstract void processData();
+    public abstract void printData();
+}
