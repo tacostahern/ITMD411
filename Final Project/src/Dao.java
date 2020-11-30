401e0d52d5ac9b50f13269199d8905b1b2c444bf
tacostahern@gmail.com <tacostahern@gmail.com>
2020-11-29 23:54:05 -0600

    Added userList.csv with updated info for me

--- Final Project/src/Dao.java
+++ Final Project/src/Dao.java
@@ -94,8 +94,7 @@ public class Dao {
 			// and PASS (insert) that data into your User table
 			for (List<String> rowData : array) {
 
-				sql = "insert into jpapa_users(uname,upass,admin) " + "values('" + rowData.get(0) + "'," + " '"
-						+ rowData.get(1) + "','" + rowData.get(2) + "');";
+				sql = "insert into tacos_users(uname,upass,admin) " + "values('" + rowData.get(0) + "'," + " '" + rowData.get(1) + "','" + rowData.get(2) + "');";
 				statement.executeUpdate(sql);
 			}
 			System.out.println("Inserts completed in the given database...");
